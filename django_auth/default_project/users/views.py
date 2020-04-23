0e3fe7a28c37173cc3c4a676265fcda13abd17c1
Vladislav Rybin <vladislavi@mail.ru>
2020-04-23 16:15:59 +0500

    second

--- django_auth/default_project/users/views.py
+++ django_auth/default_project/users/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
-    pass
+    if request.user.is_authenticated:
+        context = {
+            "user": request.user
+        }
+        return render(request, "users/user.html", context)
+    else:
+        return render(request, "users/login.html", {"message": None})
 
 
 def login_view(request):
-    pass
+    print(request.POST)
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid credentials"})
 
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
