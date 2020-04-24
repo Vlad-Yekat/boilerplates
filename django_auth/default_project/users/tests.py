8091e46d5212bad7516efd433623a184069fa129
Vladislav Rybin <vladislavi@mail.ru>
2020-04-24 13:15:26 +0500

    test

--- django_auth/default_project/users/tests.py
+++ django_auth/default_project/users/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class TestIndexPage(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
